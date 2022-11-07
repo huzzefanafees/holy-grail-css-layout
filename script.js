911d3a025e00e9df0312f27cbc4345e825d21432
huzzefanafees <huzzefanafees@gmail.com>
2022-11-07 13:09:08 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleClass(){
+const outerGrid = document.querySelector(".outer-grid");
+outerGrid.classList.toggle("outergrid-expanded");
+const nav = document.querySelector("nav");
+nav.classList.toggle("hide-nav");
+}
